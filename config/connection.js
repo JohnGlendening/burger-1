4ddfd7568bbd8d5f020436a62f44cb2a91abf8a6
alinavo <alinahuynhvo@gmail.com>
2019-02-28 22:13:16 -0500

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,25 @@
 var mysql = require("mysql");    
+var connection;
 
 //creating connection
-var connection = mysql.createConnection({ // Set connection parameters
-  host: "localhost",
-  user: "root",
-  password: "0118488320613",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({ // Set connection parameters
+//   host: "localhost",
+//   user: "root",
+//   password: "0118488320613",
+//   database: "burgers_db"
+// });
 
-// var connection;
-// if(process.env.JAWSDB_URL) {            //For Heroku Deployment
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({ // Set local connection parameters
-//     host: "localhost",
-//     user: "root",
-//     password: "0118488320613",
-//     database: "burgers_db"
-//     });
-// };
+var connection;
+if(process.env.JAWSDB_URL) {            
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({ // Set local connection parameters
+    host: "localhost",
+    user: "root",
+    password: "0118488320613",
+    database: "burgers_db"
+    });
+};
 
 //creating connection
 connection.connect(function(err) {     
